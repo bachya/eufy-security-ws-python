e617df82053119f9464e55f99e1400b9a218ac3f
Aaron Bach <bachya1208@gmail.com>
2021-07-13 17:05:34 -0600

    Fix tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,14 +1,17 @@
 """Test the server version helper."""
 from unittest.mock import call
 
-from eufy_security_ws_python.version import get_server_version
+import pytest
+
+from eufy_security_ws_python.version import async_get_server_version
 
 
-async def test_get_server_version(client_session, ws_client, url, version_data):
+@pytest.mark.asyncio
+async def test_async_get_server_version(client_session, ws_client, url, version_data):
     """Test the get server version helper."""
     ws_client.receive_json.return_value = version_data
 
-    version_info = await get_server_version(url, client_session)
+    version_info = await async_get_server_version(url, client_session)
 
     assert client_session.ws_connect.called
     assert client_session.ws_connect.call_args == call(url)
@@ -19,6 +22,7 @@ async def test_get_server_version(client_session, ws_client, url, version_data):
     assert ws_client.close.called
 
 
+@pytest.mark.asyncio
 async def test_missing_server_schema_version(
     client_session, ws_client, url, version_data
 ):
@@ -26,7 +30,7 @@ async def test_missing_server_schema_version(
     del version_data["minSchemaVersion"]
     del version_data["maxSchemaVersion"]
     ws_client.receive_json.return_value = version_data
-    version_info = await get_server_version(url, client_session)
+    version_info = await async_get_server_version(url, client_session)
     assert version_info.min_schema_version == 0
     assert version_info.max_schema_version == 0
     assert ws_client.close.called
